43e0ead757ce83b0ac3df8707c3aabcd76204c0b
Idelso Quintero <idelsoq@gmail.com>
2012-08-17 21:24:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wirelesstuner::Application.config.secret_token = '9cd4210967851b1917ddc0051675b35adff0343b6a2f02966ccd20e42333e335b29393e22b9094d6d12710be9021eb3870494b2dde6485623843f864563d21df'
